6a4afd53786d56e2ceb12755a657e3d1b3ddc531
Lucas Barros <lucas.mr.barros@live.com>
2025-01-08 14:00:38 -0300

    Add score display and scoring logic for player and computer

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 const choices = ['rock', 'paper', 'scissor']
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
+let playerScore = 0;
+let computerScore = 0;
 
 function cleanUp(){
     resultDisplay.classList.remove("greenText", "redText");
@@ -14,11 +19,17 @@ function cleanUp(){
 function playerWin(){
     resultDisplay.textContent = "Player Wins!";
     resultDisplay.classList.add("greenText");
+
+    playerScore += 1;
+    playerScoreDisplay.textContent = `${playerScore}`;
 }
 
 function computerWin(){
     resultDisplay.textContent = "Computer Wins!";
     resultDisplay.classList.add("redText");
+
+    computerScore += 1;
+    computerScoreDisplay.textContent = `${computerScore}`;
 }
 
 function tie(){
